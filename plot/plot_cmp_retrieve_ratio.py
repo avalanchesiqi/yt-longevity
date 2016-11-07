2619001658c0c780e0aa4a2a63e6359f2b9a10b1
avalanchesiqi <wusiqi.china@gmail.com>
2016-11-07 19:48:01 +1100

    handle bad case when fitting multiple lines and new figure

--- plot/plot_cmp_retrieve_ratio.py
+++ plot/plot_cmp_retrieve_ratio.py
@@ -1,16 +1,10 @@
 import os
 from datetime import datetime, timedelta
+from collections import deque
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.ticker import FuncFormatter
 import cPickle as pickle
-import logging
-
-file_loc = '../../data/test_sampling'
-
-
-def lst2num(lst):
-    return map(int, lst.split(', '))
 
 
 def get_retrieve_num(path):
@@ -29,171 +23,192 @@ def get_retrieve_num(path):
     return retrieve_dict
 
 
-def get_miss_num_max(path):
-    miss_dict = {}
-    with open(path, 'r') as data:
-        for line in data:
-            _, _, date, segments = line.rstrip().split(':')
-            print '------------------------'
-            print date
-            segments = segments.split('-')[:-1]
-            n = len(segments)
-            miss_num = max(lst2num(segments[0])) - min(lst2num(segments[0]))
-            if n > 1:
-                i = 1
-                while i < n:
-                    miss_num += max(lst2num(segments[i]))
-                    i += 1
-            print 'total miss', miss_num
-            miss_dict[date] = miss_num
-    return miss_dict
-
-
-def fit_multiple_lines(lst, truncated=False, prev=0):
-    lst = map(int, lst.split(','))
+def fit_multiple_lines(lst, head=0, prev=0):
     lines = [[lst[0]]]
-    orders = [[1]]
+    orders = [[1+prev]]
     n = len(lst)
     cnt = 2+prev
     for i in xrange(1, n):
+        comer = lst[i]
         m = len(lines)
         last_elms = np.array([lines[j][-1] for j in xrange(m)])
-        if lst[i] <= min(last_elms):
-            lines.append([lst[i]])
-            orders.append([cnt])
+        if comer <= min(last_elms):
+            if head == 0 or comer < 100:
+                lines.append([comer])
+                orders.append([cnt])
         else:
-            last_elms[lst[i] <= last_elms] = min(last_elms)-1
-            k = np.argmin(lst[i]-last_elms)
-            lines[k].append(lst[i])
+            last_elms[comer <= last_elms] = 0
+            k = np.argmin(comer-last_elms)
+            # detect unusual increment
+            lines[k].append(comer)
             orders[k].append(cnt)
         cnt += 1
-    # print 'number of lines: {0}'.format(len(lines))
-    # print lines
     t = len(lines)
-    print 'inner segments', t, len(orders)
-    return orders, lines
-    # logging.debug(t)
-    # if not t == 4:
-    #     logging.debug(lines)
-    # res = 0
-    # # print 'first: {0}, last: {1}, min: {2}, max: {3}'.format(lines[0][0], lines[0][-1], min(lines[0]), max(lines[0]))
-    # for i in xrange(t):
-    #     if truncated:
-    #         res += (lines[i][-1] - lines[i][0])
-    #         # print lines[i][-1], lines[i][0]
-    #     else:
-    #         res += lines[i][-1]
-    #         # print lines[i][-1]
-    #     # print 'last: {0}, max: {1}'.format(lines[0][-1], max(lines[0]))
-    # return res
+    print 'number of inner segments:', t, len(orders)
+    if head == 0:
+        miss_num = sum([(lines[i][-1] - lines[i][0]) for i in xrange(t)])
+    else:
+        miss_num = sum([(lines[i][-1]) for i in xrange(t)])
+    return orders, lines, miss_num
 
 
-def get_miss_num_sum(path):
-    miss_dict = {}
-    with open(path, 'r') as data:
-        data.readline()
-        ts_track = data.readline()
-        segments = ts_track.rstrip().rsplit(':', 1)[1]
-        prev = 0
-        cnt = 0
-        orders, lines = fit_multiple_lines(segments, prev=prev)
-        for i in xrange(len(orders)):
-            ax2.plot(orders[i], lines[i], '-', marker='x', ms=2)
-        prev = sum([len(order) for order in orders])
-        # for line in data:
-        #     _, _, date, segments = line.rstrip().split(':')
-        #     if date == '2016-06-09':
-        #         segments = segments.split('-')[:-1]
-        #         prev = 0
-        #         cnt = 0
-        #         for seg in segments:
-        #             # seg = map(int, seg.split(','))
-        #             # xaxis = [prev + i for i in xrange(1, len(seg)+1)]
-        #             # print len(xaxis)
-        #             # print len(seg)
-        #             # ax2.scatter(xaxis, seg, marker='x', s=1, color='rgb'[cnt])
-        #             # prev += len(seg)
-        #             # cnt += 1
-        #             orders, lines = fit_multiple_lines(seg, prev=prev)
-        #             for i in xrange(len(orders)):
-        #                 ax2.plot(orders[i], lines[i], '-', marker='x', ms=2)
-        #             prev = sum([len(order) for order in orders])
-            # print '------------------------'
-            # print date
-            # segments = segments.split('-')[:-1]
-            # miss_num = 0
-            # print 'number of connection', len(segments)
-            # n = len(segments)
-            # miss_num += fit_multiple_lines(segments[0], True)
-            # for i in xrange(1, n):
-            #     miss_num += fit_multiple_lines(segments[i], False)
-            # print 'total miss', miss_num
-            # miss_dict[date] = miss_num
-    # return miss_dict
+def get_miss_num_max(lst, head=0):
+    if head == 0:
+        miss_num = max(lst) - min(lst)
+    else:
+        miss_num = max(lst)
+    return miss_num
+
+
+def get_miss_num_controller(arrs):
+    prev = 0
+    total_miss_sum = 0
+    total_miss_max = 0
+    for (cnt, arr) in enumerate(arrs):
+        total_miss_max += get_miss_num_max(arr, head=cnt)
+
+        orders, _, segs_miss_num = fit_multiple_lines(arr, head=cnt, prev=prev)
+        prev += sum([len(order) for order in orders])
+        total_miss_sum += segs_miss_num
+    return total_miss_max, total_miss_sum
+
+
+def get_disconnect_window(arr, width):
+    box = deque(maxlen=width)
+    windows = []
+    for val in arr:
+        if len(box) == 0:
+            windows.append([val])
+        elif len(box) < width and (min(box)-val) > 2500 and val < 100:
+            # detect drop 1, not full, decrease sharply, hard threshold
+            print 'detect drop #1: {0} {1} {2} {3}'.format(val, len(box), min(box), box)
+            # reset box
+            box = deque(maxlen=width)
+            windows.append([val])
+        elif len(box) == width and min(box) > val and val < 100:
+            # detect drop 2, full, smaller than min in box, hard threshold
+            print 'detect drop #2: {0} {1} {2} {3}'.format(val, len(box), min(box), box)
+            # reset box
+            box = deque(maxlen=width)
+            windows.append([val])
+        else:
+            windows[-1].append(val)
+        box.append(val)
+
+    n = len(windows)
+    print 'number of connections:', n
+    return windows
+
+
+def get_miss_num(loc):
+    miss_max_dict = {}
+    miss_sum_dict = {}
+
+    for subdir, _, files in os.walk(loc):
+        for f in sorted(files):
+            path = os.path.join(subdir, f)
+            tracks = []
+            with open(path, 'r') as data:
+                for line in data:
+                    track = int(line.rstrip())
+                    tracks.append(track)
+
+            date = path.rstrip()[-18:-8]
+            print '-----------------'
+            print date
+            width = 50
+            non_disconnect_windows = get_disconnect_window(tracks, width)
+            total_miss_max, total_miss_sum = get_miss_num_controller(non_disconnect_windows)
+
+            miss_max_dict[date] = total_miss_max
+            miss_sum_dict[date] = total_miss_sum
+    return miss_max_dict, miss_sum_dict
 
 
 def date2str(date_format):
     return date_format.strftime("%Y-%m-%d")
 
 
-def millions(x):
+def millions(x, pos):
     return '%1.1fM' % (x*1e-6)
 
 
+def get_stats(arr):
+    arr = np.array(arr)
+    return np.mean(arr), np.std(arr), np.min(arr), np.percentile(arr, 25), np.median(arr), np.percentile(arr, 75), np.max(arr)
+
+
+def plot_stats_controller(ax):
+    after = [datetime(2015, 2, 7)+timedelta(days=i) for i in xrange(150)]
+    before = [datetime(2015, 2, 5)+timedelta(days=-i) for i in xrange(150)]
+    retrieve_before = [retrieve_map[date2str(d)] for d in before if date2str(d) in retrieve_map]
+    max_before = [retrieve_map[date2str(d)] + miss_max_map[date2str(d)] for d in before if date2str(d) in retrieve_map and date2str(d) in miss_max_map]
+    sum_before = [retrieve_map[date2str(d)] + miss_sum_map[date2str(d)] for d in before if date2str(d) in retrieve_map and date2str(d) in miss_sum_map]
+    retrieve_after = [retrieve_map[date2str(d)] for d in after if date2str(d) in retrieve_map]
+    max_after = [retrieve_map[date2str(d)] + miss_max_map[date2str(d)] for d in after if date2str(d) in retrieve_map and date2str(d) in miss_max_map]
+    sum_after = [retrieve_map[date2str(d)] + miss_sum_map[date2str(d)] for d in after if date2str(d) in retrieve_map and date2str(d) in miss_sum_map]
+    print get_stats(retrieve_before)
+    print get_stats(max_before)
+    print get_stats(sum_before)
+    print get_stats(retrieve_after)
+    print get_stats(max_after)
+    print get_stats(sum_after)
+
+
 if __name__ == '__main__':
-    # fig, (ax1, ax2) = plt.subplots(2, 1)
-    # fig, (ax1, ax2, ax3) = plt.subplots(1, 3)
-    fig, ax2 = plt.subplots(1, 1)
+    fig, (ax1, ax2) = plt.subplots(2, 1)
 
-    # logging.basicConfig(filename='../../data/test_sampling/inner_segments.log', level=logging.DEBUG)
-    #
-    # filepath1 = os.path.join(file_loc, 'sample_ratio.log')
-    filepath2 = os.path.join(file_loc, 'tweet_rate_ts.log')
-    get_miss_num_sum(filepath2)
+    file_loc1 = '../../data/test_sampling'
+    file_loc2 = '../../data/rate_limit'
+    filepath1 = os.path.join(file_loc1, 'sample_ratio.log')
 
-    #
     # retrieve_map = get_retrieve_num(filepath1)
-    # miss_max_map = get_miss_num_max(filepath2)
-    # miss_sum_map = get_miss_num_sum(filepath2)
-    #
-    # with open(os.path.join(file_loc, r'retrieve_map.pickle'), 'wb') as output1:
+    # miss_max_map, miss_sum_map = get_miss_num(file_loc2)
+
+    # with open(os.path.join(file_loc1, r'retrieve_map.pickle'), 'wb') as output1:
     #     pickle.dump(retrieve_map, output1)
-    # with open(os.path.join(file_loc, r'miss_max_map.pickle'), 'wb') as output2:
+    # with open(os.path.join(file_loc1, r'miss_max_map.pickle'), 'wb') as output2:
     #     pickle.dump(miss_max_map, output2)
-    # with open(os.path.join(file_loc, r'miss_sum_map.pickle'), 'wb') as output3:
+    # with open(os.path.join(file_loc1, r'miss_sum_map.pickle'), 'wb') as output3:
     #     pickle.dump(miss_sum_map, output3)
 
-    # with open(os.path.join(file_loc, r'retrieve_map.pickle'), 'rb') as input1:
-    #     retrieve_map = pickle.load(input1)
-    # with open(os.path.join(file_loc, r'miss_max_map.pickle'), 'rb') as input2:
-    #     miss_max_map = pickle.load(input2)
-    # with open(os.path.join(file_loc, r'miss_sum_map.pickle'), 'rb') as input3:
-    #     miss_sum_map = pickle.load(input3)
-    #
-    # del retrieve_map['2016-04-03']
-    # date_axis = [datetime(2014, 6, 1)+timedelta(days=i) for i in xrange(len(retrieve_map))]
-    # retrieve_axis = [retrieve_map[date2str(d)] if date2str(d) in retrieve_map else np.nan for d in date_axis]
-    # restore_axis1 = [retrieve_map[date2str(d)]+miss_max_map[date2str(d)] if date2str(d) in retrieve_map and date2str(d) in miss_max_map else np.nan for d in date_axis]
-    # restore_axis2 = [retrieve_map[date2str(d)]+miss_sum_map[date2str(d)] if date2str(d) in retrieve_map and date2str(d) in miss_max_map else np.nan for d in date_axis]
-    # ax1.plot_date(date_axis, restore_axis1, '-', ms=2, color='b', label='assume max difference')
-    # ax1.plot_date(date_axis, restore_axis2, '-', ms=2, color='r', label='assume multiple lines sum up')
-    # ax1.plot_date(date_axis, retrieve_axis, '-', ms=2, color='g', label='practical retrieve number')
-    # ax1.bar(date_axis, np.isnan(retrieve_axis)*ax1.get_ylim()[1], color=(.9, .9, .9))
-    # ax1.yaxis.set_major_formatter(FuncFormatter(millions))
-    # ax1.plot_date((datetime(2015,2,6), datetime(2015,2,6)), (0, ax1.get_ylim()[1]), 'm-')
-    # ax1.set_xlabel('Date')
-    # ax1.set_ylabel('Retrieve tweets number')
-    # ax1.legend(loc='lower left')
-    #
-    # sampling_axis1 = [100.0*retrieve_map[date2str(d)]/(retrieve_map[date2str(d)]+miss_max_map[date2str(d)]) if date2str(d) in retrieve_map and date2str(d) in miss_max_map else np.nan for d in date_axis]
-    # sampling_axis2 = [100.0*retrieve_map[date2str(d)]/(retrieve_map[date2str(d)]+miss_sum_map[date2str(d)]) if date2str(d) in retrieve_map and date2str(d) in miss_max_map else np.nan for d in date_axis]
-    # ax2.plot_date(date_axis, sampling_axis1, '-', ms=2, color='b', label='assume max difference')
-    # ax2.plot_date(date_axis, sampling_axis2, '-', ms=2, color='r', label='assume multiple lines sum up')
-    # ax2.bar(date_axis, np.isnan(retrieve_axis) * ax2.get_ylim()[1], color=(.9, .9, .9))
-    # ax2.plot_date((datetime(2015, 2, 6), datetime(2015, 2, 6)), (0, ax2.get_ylim()[1]), 'm-')
-    # ax2.set_xlabel('Date')
-    # ax2.set_ylabel('Retrieve ratio')
-    # ax2.legend(loc='lower left')
+    with open(os.path.join(file_loc1, r'retrieve_map.pickle'), 'rb') as input1:
+        retrieve_map = pickle.load(input1)
+    with open(os.path.join(file_loc1, r'miss_max_map.pickle'), 'rb') as input2:
+        miss_max_map = pickle.load(input2)
+    with open(os.path.join(file_loc1, r'miss_sum_map.pickle'), 'rb') as input3:
+        miss_sum_map = pickle.load(input3)
+
+    # del retrieve_map['2014-05-28']
+    del retrieve_map['2014-12-26']
+    del retrieve_map['2016-04-03']
+    date_axis = [datetime(2014, 6, 1)+timedelta(days=i) for i in xrange(len(retrieve_map)+115)]
+    retrieve_axis = [retrieve_map[date2str(d)] if date2str(d) in retrieve_map else np.nan for d in date_axis]
+    restore_axis1 = [retrieve_map[date2str(d)]+miss_max_map[date2str(d)] if date2str(d) in retrieve_map and date2str(d) in miss_max_map else np.nan for d in date_axis]
+    restore_axis2 = [retrieve_map[date2str(d)]+miss_sum_map[date2str(d)] if date2str(d) in retrieve_map and date2str(d) in miss_sum_map else np.nan for d in date_axis]
+    ax1.plot_date(date_axis, restore_axis1, '-', ms=2, color='b', label='assume max difference')
+    ax1.plot_date(date_axis, restore_axis2, '-', ms=2, color='r', label='assume multiple lines sum up')
+    ax1.plot_date(date_axis, retrieve_axis, '-', ms=2, color='g', label='ground-truth retrieve number')
+    ax1.bar(date_axis, np.isnan(retrieve_axis)*ax1.get_ylim()[1], color=(.9, .9, .9))
+    ax1.yaxis.set_major_formatter(FuncFormatter(millions))
+    ax1.plot_date((datetime(2015,2,6), datetime(2015,2,6)), (0, ax1.get_ylim()[1]), 'm-')
+
+    # plot mean, std, five points summary
+    plot_stats_controller(ax1)
+
+    ax1.set_xlabel('Date')
+    ax1.set_ylabel('Retrieve tweets number')
+    ax1.legend(loc='lower left')
+
+    sampling_axis1 = [100.0*retrieve_map[date2str(d)]/(retrieve_map[date2str(d)]+miss_max_map[date2str(d)]) if date2str(d) in retrieve_map and date2str(d) in miss_max_map else np.nan for d in date_axis]
+    sampling_axis2 = [100.0*retrieve_map[date2str(d)]/(retrieve_map[date2str(d)]+miss_sum_map[date2str(d)]) if date2str(d) in retrieve_map and date2str(d) in miss_max_map else np.nan for d in date_axis]
+    ax2.plot_date(date_axis, sampling_axis1, '-', ms=2, color='b', label='assume max difference')
+    ax2.plot_date(date_axis, sampling_axis2, '-', ms=2, color='r', label='assume multiple lines sum up')
+    ax2.bar(date_axis, np.isnan(retrieve_axis) * ax2.get_ylim()[1], color=(.9, .9, .9))
+    ax2.plot_date((datetime(2015, 2, 6), datetime(2015, 2, 6)), (0, ax2.get_ylim()[1]), 'm-')
+    ax2.set_xlabel('Date')
+    ax2.set_ylabel('Retrieve ratio')
+    ax2.legend(loc='lower left')
 
     # plt.tight_layout()
     plt.show()
--- plot/plot_rate_ts_value.py
+++ plot/plot_rate_ts_value.py
@@ -1,52 +1,106 @@
 import os
+from collections import deque
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
-import matplotlib.dates as mdate
-import seaborn as sns
 
-file_loc = '../../data/test_sampling'
+file_loc = '../../data/rate_limit'
 
 
-def isincrease(lst):
+def fit_multiple_lines(lst, head=0, prev=0):
+    lines = [[lst[0]]]
+    orders = [[1+prev]]
     n = len(lst)
-    cnt = 0
-    print n
-    for i in xrange(n-1):
-        for j in xrange(i+1, n):
-            if lst[j] < lst[i]:
-                print i-j
-                print lst[i] - lst[j]
-                cnt += 1
-                print '----------------'
-            # return False
-    print cnt
-    return
+    cnt = 2+prev
+    for i in xrange(1, n):
+        comer = lst[i]
+        m = len(lines)
+        last_elms = np.array([lines[j][-1] for j in xrange(m)])
+        if comer <= min(last_elms):
+            if head == 0 or comer < 100:
+                lines.append([comer])
+                orders.append([cnt])
+        else:
+            last_elms[comer <= last_elms] = 0
+            k = np.argmin(comer-last_elms)
+            # detect unusual increment
+            lines[k].append(comer)
+            orders[k].append(cnt)
+        cnt += 1
+    t = len(lines)
+    print 'number of inner segments:', t, len(orders)
+    if head == 0:
+        miss_num = sum([(lines[i][-1] - lines[i][0]) for i in xrange(t)])
+    else:
+        miss_num = sum([(lines[i][-1]) for i in xrange(t)])
+    return orders, lines, miss_num
 
 
-def read_data(filepath):
-    skip = 16
-    with open(filepath, 'r') as filedata:
-        for _ in xrange(skip):
-            filedata.readline()
-            filedata.readline()
-        values = filedata.readline().rstrip().split(':')[-1]
-        values = map(int, values.split(','))
-        tss = filedata.readline().rstrip().split(':')[-1]
-        # print isincrease(map(int, tss.split(',')))
-        tss = map(int, tss.split(','))
-        # tss = np.array(map(int, tss.split(',')))/1000
-        # tss = mdate.epoch2num(tss)
-    df = pd.DataFrame({'timestamp': tss[:1000], 'value': values[:1000]})
-    return df
+def get_miss_num_sum(arrs, ax):
+    prev = 0
+    total_miss = 0
+    for (cnt, arr) in enumerate(arrs):
+        orders, segs, segs_miss_num = fit_multiple_lines(arr, head=cnt, prev=prev)
+        prev += sum([len(order) for order in orders])
+        total_miss += segs_miss_num
+        for order, seg in zip(orders, segs):
+            ax.plot(order, seg, '-', ms=2, marker='x')
+        ax.set_title('Simulation result of assigning rate limit message to nearby line')
+        ax.set_xlabel('Incoming order')
+    print 'sum total miss:', total_miss
+
+
+def get_disconnect_window(arr, width):
+    box = deque(maxlen=width)
+    windows = []
+    for val in arr:
+        if len(box) == 0:
+            windows.append([val])
+        elif len(box) < width and (min(box)-val) > 2500 and val < 100:
+            # detect drop 1, not full, decrease sharply, hard threshold
+            print 'detect drop #1: {0} {1} {2} {3}'.format(val, len(box), min(box), box)
+            # reset box
+            box = deque(maxlen=width)
+            windows.append([val])
+        elif len(box) == width and min(box) > val and val < 100:
+            # detect drop 2, full, smaller than min in box, hard threshold
+            print 'detect drop #2: {0} {1} {2} {3}'.format(val, len(box), min(box), box)
+            # reset box
+            box = deque(maxlen=width)
+            windows.append([val])
+        else:
+            windows[-1].append(val)
+        box.append(val)
+
+    n = len(windows)
+    print 'number of connections:', n
+    return windows
+
+
+def plot_track_scatter(arr, ax):
+    """ Scatter plot of value in rate limit messages"""
+    xaxis = np.arange(1, len(arr)+1)
+    ax.scatter(xaxis, arr, marker='x', s=1, c='b')
+    ax.set_title('Scatter plot of incoming order wrt rate limit message value')
+    ax.set_xlim(xmin=0)
+    ax.set_ylim(ymin=0)
+    ax.set_xlabel('Incoming order')
+    ax.set_ylabel('Rate limit value')
+
 
 if __name__ == '__main__':
-    filepath = os.path.join(file_loc, 'maximum_invert_ts.log')
-    df1 = read_data(filepath)
-    # df2 = read_data(filepath2)
-    sns.jointplot(x='timestamp', y='value', data=df1, kind='reg', joint_kws={'line_kws': {'color': 'red'}})
-    # sns.jointplot(x='timestamp', y='value', data=df1)
-    # sns.jointplot(x='index', y='timestamp', data=df2, kind='reg', joint_kws={'line_kws': {'color': 'red'}})
-    # sns.regplot(x='index', y='timestamp', data=df2, fit_reg=True, ax=ax2)
-    sns.despine()
-    sns.plt.show()
+    # 2015-04-09
+    filepath = os.path.join(file_loc, 'rate_2016-07-21.bz2.txt')
+
+    fig, (ax1, ax2) = plt.subplots(1, 2, sharex=True, sharey=True)
+    tracks = []
+    with open(filepath, 'r') as filedata:
+        for line in filedata:
+            track = int(line.rstrip())
+            tracks.append(track)
+    plot_track_scatter(tracks, ax1)
+
+    width = 50
+    non_disconnect_windows = get_disconnect_window(tracks, width)
+    get_miss_num_sum(non_disconnect_windows, ax2)
+
+    plt.show()
