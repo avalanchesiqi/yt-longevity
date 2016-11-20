2ddf8416c687a6561b75d695ebc603c3cff9d0cb
“avalanchesiqi” <wusiqi.china@gmail.com>
2016-11-20 23:54:30 +1100

    update line fitting by nearest timestamp

--- plot/plot_fit_multiple_lines_by_timestamp.py
+++ plot/plot_fit_multiple_lines_by_timestamp.py
@@ -14,6 +14,18 @@ def flatten(arrs):
     return ret
 
 
+def plot_scatter(arr1, arr2, ax):
+    """ Scatter plot of track value in rate limit messages. """
+    ax.scatter(arr1, arr2, marker='x', s=1, c='b')
+    ax.set_title('Scatter plot of rate limit message')
+    ax.set_xlim(xmin=min(arr1))
+    ax.set_xlim(xmax=max(arr1))
+    ax.set_ylim(ymin=min(arr2))
+    ax.set_ylim(ymax=max(arr2))
+    ax.set_xlabel('UTC datetime')
+    ax.set_ylabel('Rate limit message value')
+
+
 def fit_and_plot_multiple_lines(rate_arr, ts_arr, ax):
     """Separate an array into multiple monotonic arrays.
     """
@@ -49,7 +61,6 @@ def fit_and_plot_multiple_lines(rate_arr, ts_arr, ax):
                 for _ in xrange(n-len(monotonic_line)):
                     monotonic_line.append(dup_rate)
 
-    ts_arr = [datetime.utcfromtimestamp(ts) for ts in ts_arr]
     for monotonic_line in monotonic_lines:
         ax.plot_date(ts_arr, monotonic_line, '-', ms=2, marker='x')
 
@@ -60,29 +71,38 @@ def fit_and_plot_multiple_lines(rate_arr, ts_arr, ax):
 
 
 if __name__ == '__main__':
-    file_loc = '../../data'
-    filename = '2016-06-23-2.txt'
+    file_loc = '../../data/twitter-sampling'
+    filename = 'one_hour_rate.txt'
     filepath = os.path.join(file_loc, filename)
 
-    rate_arr = []
     ts_arr = [0]
+    rate_arr = []
+    raw_ts_arr = []
+    raw_rate_arr = []
     with open(filepath, 'r') as filedata:
         for line in filedata:
             msg = json.loads(line.rstrip())
+            ts = int(round(1.0 * int(msg['limit']['timestamp_ms']) / 1000))
+            ts = datetime.utcfromtimestamp(ts)
             rate = int(msg['limit']['track'])
-            ts = int(round(1.0*int(msg['limit']['timestamp_ms'])/1000))
             if not ts_arr[-1] == ts:
-                rate_arr.append([rate])
                 ts_arr.append(ts)
+                rate_arr.append([rate])
             else:
                 rate_arr[-1].append(rate)
+            raw_ts_arr.append(ts)
+            raw_rate_arr.append(rate)
 
     ts_arr.pop(0)
-    fig, ax1 = plt.subplots(1, 1)
+    fig, (ax1, ax2) = plt.subplots(1, 2, sharex=True, sharey=True)
+    plt.setp(ax1.get_xticklabels(), rotation=45)
+    plt.setp(ax2.get_xticklabels(), rotation=45)
 
-    fit_and_plot_multiple_lines(rate_arr, ts_arr, ax1)
+    plot_scatter(raw_ts_arr, raw_rate_arr, ax1)
 
-    # for i in xrange(ts_arr[0], ts_arr[-1]+1):
+    fit_and_plot_multiple_lines(rate_arr, ts_arr, ax2)
+
+    # for i in xrange(int(mdate.date2num(ts_arr[0])), int(mdate.date2num(ts_arr[-1]))+1):
     #     if i not in ts_arr:
     #         print datetime.utcfromtimestamp(i)
 
