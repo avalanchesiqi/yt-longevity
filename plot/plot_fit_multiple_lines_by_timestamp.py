ff8e3083a1f30d75dd02971bbe58351af47bf1ff
avalanchesiqi <wusiqi.china@gmail.com>
2016-12-02 17:18:28 +1100

    update line fitting to nearby second, handle disconnect

--- plot/plot_fit_multiple_lines_by_timestamp.py
+++ plot/plot_fit_multiple_lines_by_timestamp.py
@@ -1,17 +1,9 @@
 import os
 from datetime import datetime
 import json
+from collections import deque
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdate
-
-
-def flatten(arrs):
-    """ Flatten list of arrays in to an array.
-    """
-    ret = []
-    for arr in arrs:
-        ret.extend(arr)
-    return ret
 
 
 def plot_scatter(arr1, arr2, ax):
@@ -26,6 +18,34 @@ def plot_scatter(arr1, arr2, ax):
     ax.set_title('Figure 3: Scatter plot of rate limit messages.', y=-0.19)
 
 
+def get_non_disconnect_window(arr, width):
+    """ Get non-disconnect windows from rate limit messages. """
+    box = deque(maxlen=width)
+    windows = []
+    for val in arr:
+        if len(box) == 0:
+            windows.append([val])
+        elif len(box) < width and (min(box)-val) > 2500 and val < 300:
+            # detect drop 1, not full, decrease sharply, hard threshold
+            print 'detect drop #1: {0} {1} {2} {3}'.format(val, len(box), min(box), box)
+            # reset box
+            box = deque(maxlen=width)
+            windows.append([val])
+        elif len(box) == width and val < min(box) and val < 300:
+            # detect drop 2, full, smaller than min in box, hard threshold
+            print 'detect drop #2: {0} {1} {2} {3}'.format(val, len(box), min(box), box)
+            # reset box
+            box = deque(maxlen=width)
+            windows.append([val])
+        else:
+            windows[-1].append(val)
+        box.append(val)
+
+    n1 = len(windows)
+    print 'number of connections:', n1
+    return windows
+
+
 def fit_and_plot_multiple_lines(rate_arr, ts_arr, ax):
     """Separate an array into multiple monotonic arrays.
     """
@@ -36,27 +56,29 @@ def fit_and_plot_multiple_lines(rate_arr, ts_arr, ax):
         if len(monotonic_lines) == 0:
             monotonic_lines = [[rate] for rate in cur_rates]
         else:
-            prev_rates = [monotonic_line[-1] for monotonic_line in monotonic_lines]
-            m = len(prev_rates)
-            for i, rate in enumerate(cur_rates):
-                if rate <= min(prev_rates):
-                    monotonic_lines.append([0] * k)
-                    monotonic_lines[-1].append(rate)
-                else:
-                    for j in xrange(i, m):
-                        if rate > prev_rates[j]:
-                            monotonic_lines[j].append(rate)
-                            prev_rates[j] = rate
-                            break
-                        elif len(monotonic_lines[j]) < k:
-                            monotonic_lines[j].append(prev_rates[j])
+            if len(cur_rates) <= 4:
+                prev_rates = [monotonic_line[-1] for monotonic_line in monotonic_lines]
+                m = len(prev_rates)
+                for i, rate in enumerate(cur_rates):
+                    if rate <= min(prev_rates) and k < 100:
+                        # fill with 0 backwards
+                        monotonic_lines.append([0] * k)
+                        monotonic_lines[-1].append(rate)
+                    else:
+                        for j in xrange(i, m):
+                            if rate > prev_rates[j]:
+                                monotonic_lines[j].append(rate)
+                                prev_rates[j] = rate
+                                break
+                            elif len(monotonic_lines[j]) < k:
+                                monotonic_lines[j].append(prev_rates[j])
 
             for monotonic_line in monotonic_lines:
                 if not len(monotonic_line) == k+1:
                     monotonic_line.append(monotonic_line[-1])
 
     for monotonic_line in monotonic_lines:
-        ax.plot_date(ts_arr[5:], monotonic_line[5:], '-', ms=2, marker='x')
+        ax.plot_date(ts_arr, monotonic_line, '-', ms=2, marker='x')
 
     xfmt = mdate.DateFormatter('%H:%M:%S')
     ax.xaxis.set_major_formatter(xfmt)
@@ -66,11 +88,9 @@ def fit_and_plot_multiple_lines(rate_arr, ts_arr, ax):
 
 if __name__ == '__main__':
     file_loc = '../../data/twitter-sampling'
-    filename = '2016-10-14_inactive_rate.txt'
+    filename = 'dml_rate.txt'
     filepath = os.path.join(file_loc, filename)
 
-    ts_arr = [0]
-    rate_arr = []
     raw_ts_arr = []
     raw_rate_arr = []
     with open(filepath, 'r') as filedata:
@@ -79,25 +99,38 @@ if __name__ == '__main__':
             ts = int(round(1.0 * int(msg['limit']['timestamp_ms']) / 1000))
             ts = datetime.utcfromtimestamp(ts)
             rate = int(msg['limit']['track'])
+            raw_ts_arr.append(ts)
+            raw_rate_arr.append(rate)
+
+    width = 20
+    non_disconnect_windows = get_non_disconnect_window(raw_rate_arr, width)
+
+    length_arr = [len(window) for window in non_disconnect_windows]
+    non_disconnect_timestamps = []
+    prev_index = 0
+    for i in xrange(len(length_arr)):
+        non_disconnect_timestamps.append(raw_ts_arr[prev_index: prev_index+length_arr[i]])
+        prev_index += length_arr[i]
+
+    fig, (ax1, ax2) = plt.subplots(1, 2, sharex=True, sharey=True)
+
+    plot_scatter(raw_ts_arr, raw_rate_arr, ax1)
+
+    for non_disconnect_timestamp, non_disconnect_window in zip(non_disconnect_timestamps, non_disconnect_windows):
+        ts_arr = [0]
+        rate_arr = []
+        for i in xrange(len(non_disconnect_window)):
+            ts = non_disconnect_timestamp[i]
+            rate = non_disconnect_window[i]
             if not ts_arr[-1] == ts:
                 ts_arr.append(ts)
                 rate_arr.append([rate])
             else:
                 rate_arr[-1].append(rate)
-            raw_ts_arr.append(ts)
-            raw_rate_arr.append(rate)
+        ts_arr.pop(0)
+        fit_and_plot_multiple_lines(rate_arr, ts_arr, ax2)
 
-    ts_arr.pop(0)
-    fig, (ax1, ax2) = plt.subplots(1, 2, sharex=True, sharey=True)
     plt.setp(ax1.get_xticklabels(), rotation=45)
     plt.setp(ax2.get_xticklabels(), rotation=45)
-
-    plot_scatter(raw_ts_arr, raw_rate_arr, ax1)
-
-    fit_and_plot_multiple_lines(rate_arr, ts_arr, ax2)
-
-    # for i in xrange(int(mdate.date2num(ts_arr[0])), int(mdate.date2num(ts_arr[-1]))+1):
-    #     if i not in ts_arr:
-    #         print datetime.utcfromtimestamp(i)
 
     plt.show()
