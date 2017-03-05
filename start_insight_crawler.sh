102d9f59021e3803eb6ce4393ec7e317a9baa507
avalanchesiqi <wusiqi.china@gmail.com>
2017-03-06 10:06:02 +1100

    [deploy] change relative path and fix metadata process num

--- start_insight_crawler.sh
+++ start_insight_crawler.sh
@@ -6,6 +6,8 @@ host=$(echo $(hostname) | cut -d. -f1)
 
 filename='/mnt/data/metadata_mar_2017/'$host'-meta'$1'.json'
 
+# start new job
+cd /home/ec2-user/yt-longevity/
 python /home/ec2-user/yt-longevity/yt_longevity/extract_vids.py $filename /home/ec2-user/yt-longevity/vids
 mkdir /mnt/data/insightdata_mar_2017/$host-daily$transidx
 nohup python -u /home/ec2-user/yt-longevity/run.py -f dailydata -i /home/ec2-user/yt-longevity/vids -o /mnt/data/insightdata_mar_2017/$host-daily$transidx/ > /home/ec2-user/yt-longevity/log/insightdata.log &
--- transfer_to_central.sh
+++ transfer_to_central.sh
@@ -8,7 +8,7 @@ host=$(echo $(hostname) | cut -d. -f1)
 # kill old job if not finish
 procnum=`ps -ef | grep "run.py" | grep "metadata" | grep -v grep | wc -l`
 if [ $procnum -eq 1 ]; then
-    kill $(ps -ef | grep "run.py" | grep -v grep | awk '{print $2}')
+    kill $(ps -ef | grep "run.py" | grep "metadata" | grep -v grep | awk '{print $2}')
     let oldidx=$(cat $idxfile)
     oldfilename='/mnt/data/metadata_mar_2017/'$host'-meta'$oldidx'.json'
     tmpfilename='/mnt/data/metadata_mar_2017/'$host'-metatmp.json'
@@ -18,19 +18,15 @@ if [ $procnum -eq 1 ]; then
     echo $idx > $idxfile
 fi
 
-
 # get last output filename
 transidx=$(($(cat $idxfile) - 1))
 filename='/mnt/data/metadata_mar_2017/'$host'-meta'$transidx'.json'
 
-
 # sleep random second to prevent peer connection reset
 sleep $((RANDOM % 10))
 
-
 # start transform
 scp -i /home/ec2-user/yt-longevity/conf/siqwu.key $filename ec2-user@130.56.249.25:/home/ec2-user/data/metadata/crawled_mar_2017/
-
 
 # start new job
 cd /home/ec2-user/yt-longevity/
