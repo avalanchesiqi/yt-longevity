afc2e1c95529c6778d0080fb193bbfac177d727c
avalanchesiqi <Kevin5Kobe8>
2017-03-09 10:00:24 +1100

    [deploy] forget changing dir name in mkdir command

--- start_insight_crawler.sh
+++ start_insight_crawler.sh
@@ -9,5 +9,5 @@ filename='/mnt/data/metadata_mar_2017/'$host'-meta'$1'.json'
 # start new job
 cd /home/ec2-user/yt-longevity/
 python /home/ec2-user/yt-longevity/yt_longevity/extract_vids.py $filename /home/ec2-user/yt-longevity/vids
-mkdir /mnt/data/insightdata_mar_2017/$host-daily$transidx
+mkdir /mnt/data/insightdata_mar_2017/$host-daily$1
 nohup python -u /home/ec2-user/yt-longevity/run.py -f dailydata -i /home/ec2-user/yt-longevity/vids -o /mnt/data/insightdata_mar_2017/$host-daily$1/ > /home/ec2-user/yt-longevity/log/insightdata.log &
