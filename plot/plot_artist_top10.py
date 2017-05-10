11dda79deb64edaf9d1a506c2c4fe770c944afe9
avalanchesiqi <wusiqi.china@gmail.com>
2017-05-10 22:54:33 +1000

    [plot] update script to plot top 10 channel videos

--- plot/plot_artist_top10.py
+++ plot/plot_artist_top10.py
@@ -3,6 +3,9 @@
 import sys
 import os
 import operator
+import argparse
+import numpy as np
+import json
 from datetime import datetime, timedelta
 import matplotlib.pyplot as plt
 from matplotlib.ticker import FuncFormatter
@@ -14,71 +17,77 @@ def millions(x, pos):
 m_formatter = FuncFormatter(millions)
 
 
-def plot_trend(path, idx, dates):
-    with open(path, 'r') as data:
-        vid = path.rsplit('\\', 1)[1]
-        start_date, dailyview, totalview, dailyshare, totalshare = data.readline().rstrip().split()[:5]
-        y, m, d = map(int, start_date.split('-'))
-        start_date = datetime(y, m, d)
-        ax_y = map(int, dailyview.split(','))
-        ax_x = [start_date + timedelta(days=i) for i in xrange(len(ax_y))]
-        current_ax = axs[idx]
-        current_ax.plot_date(ax_x, ax_y, '-', c='b')
-        current_ax.plot((start_date, start_date), (0, current_ax.get_ylim()[1]), 'g-')
-        for date in dates:
-            if (date-start_date).days > 0:
-                current_ax.plot((date, date), (0, current_ax.get_ylim()[1]), 'r-')
-        current_ax.yaxis.set_ticks([0, current_ax.get_ylim()[1]])
-        current_ax.get_yaxis().set_major_formatter(m_formatter)
-        current_ax.text(0.01, 0.95, '{0}: {1:.2E}'.format(vid_title_dict[vid], int(totalview)), transform=axs[idx].transAxes, fontsize=12, verticalalignment='top')
+def read_as_int_array(content):
+    return np.array(map(int, content.split(',')), dtype=np.uint32)
 
 
-def get_top10_paths(dir_path):
-    path_view_dict = {}
-    for subdir, _, files in os.walk(dir_path):
-        for f in files:
-            file_path = os.path.join(subdir, f)
-            with open(file_path, 'r') as data:
-                _, _, totalview = data.readline().rstrip().split()[:3]
-                path_view_dict[file_path] = int(totalview)
-    sorted_by_view = sorted(path_view_dict.items(), key=operator.itemgetter(1), reverse=True)[:10]
-    paths = [t[0] for t in sorted_by_view]
-    return paths
+def plot_trend(ax, video, upload_dates):
+    ax_y = video.get_dailyview()
+    start_date = video.get_start_date()
+    ax_x = [start_date + timedelta(days=i) for i in xrange(len(ax_y))]
+    ax.plot_date(ax_x, ax_y, '-', c='b')
+    ax.plot((start_date, start_date), (0, ax.get_ylim()[1]), 'g-')
+    for date in upload_dates:
+        if (date-start_date).days > 0:
+            ax.plot((date, date), (0, ax.get_ylim()[1]), 'r-')
+    ax.yaxis.set_ticks([0, ax.get_ylim()[1]])
+    ax.get_yaxis().set_major_formatter(m_formatter)
+    ax.text(0.01, 0.95, '{0}: {1:.2e}'.format(video.get_title(), video.get_totalview()), transform=ax.transAxes, fontsize=12, verticalalignment='top')
 
 
-def get_dates(path):
-    with open(path, 'r') as data:
-        start_date = data.readline().rstrip().split()[0]
-        y, m, d = map(int, start_date.split('-'))
-        start_date = datetime(y, m, d)
-    return start_date
+class Video:
+    def __init__(self, content):
+        self.content = content
+
+    def __hash__(self):
+        return hash(self.content['id'])
+
+    def __eq__(self, other):
+        return self.content['id'] == other.content['id']
+
+    def get_days(self):
+        return read_as_int_array(self.content['insights']['days'])
+
+    def get_dailyview(self):
+        return read_as_int_array(self.content['insights']['dailyView'])
+
+    def get_totalview(self):
+        return int(self.content['statistics']['viewCount'])
+
+    def get_start_date(self):
+        return datetime(*map(int, self.content['insights']['startDate'].split('-')))
+
+    def get_title(self):
+        return self.content['snippet']['title']
 
 
-def plot_main(dir_path):
-    top10_paths = get_top10_paths(dir_path)
-    dates = []
-    for path in top10_paths:
-        dates.append(get_dates(path))
-    for idx, path in enumerate(top10_paths):
-        plot_trend(path, idx, dates)
+def plot_main(input_path):
+    video_view_dict = {}
+    with open(input_path, 'r') as f:
+        for line in f:
+            video_json = json.loads(line.rstrip())
+            video_obj = Video(video_json)
+            total_view = video_obj.get_totalview()
+            video_view_dict[video_obj] = total_view
+    sorted_by_view = sorted(video_view_dict.items(), key=operator.itemgetter(1), reverse=True)[:10]
+    videos = [t[0] for t in sorted_by_view]
 
+    fig, (ax1, ax2, ax3, ax4, ax5, ax6, ax7, ax8, ax9, ax10) = plt.subplots(10, 1, sharex=True)
+    axs = [ax1, ax2, ax3, ax4, ax5, ax6, ax7, ax8, ax9, ax10]
 
-def get_vid_title(path):
-    vid_title_dict = {}
-    with open(path, 'r') as data:
-        for line in data:
-            vid, _, _, title = line.rstrip().split(',', 3)
-            vid_title_dict[vid] = title
-    return vid_title_dict
+    upload_dates = [video.get_start_date() for video in videos]
+    for idx, video in enumerate(videos):
+        plot_trend(axs[idx], video, upload_dates)
 
 
 if __name__ == '__main__':
-    fig, (ax1, ax2, ax3, ax4, ax5, ax6, ax7, ax8, ax9, ax10) = plt.subplots(10, 1, sharex=True)
-    axs = [ax1, ax2, ax3, ax4, ax5, ax6, ax7, ax8, ax9, ax10]
-    channel_title = 'KatyPerryVEVO'
-    dir_path = '../output/{0}'.format(channel_title)
-    vid_title_dict = get_vid_title('../input/{0}.txt'.format(channel_title))
+    # I/O interface, read from an input file
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input', help='input file path of video metadata/insight data', required=True)
+    args = parser.parse_args()
+    input_path = args.input
 
-    plot_main(dir_path)
+    plot_main(input_path)
+
     plt.tight_layout()
     plt.show()
