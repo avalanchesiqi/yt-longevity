f9a39333209b898ea608dd161da231244479375e
avalanchesiqi <wusiqi.china@gmail.com>
2017-07-22 23:36:44 +1000

    [plot] update performance comparison plot

--- wp_prediction/plot_performance_matrix.py
+++ wp_prediction/plot_performance_matrix.py
@@ -1,5 +1,6 @@
 from __future__ import print_function, division
 import numpy as np
+from sklearn.metrics import r2_score
 import matplotlib.pyplot as plt
 
 
@@ -35,7 +36,7 @@ def get_lower_bound(bin_idx):
 
 def plot_performance_comp(matrix, fig_idx):
     ax = fig.add_subplot(321 + fig_idx)
-    ax.boxplot(matrix, labels=['Dur', 'Dur/Topic', 'Dur-N', 'Dur/Topic-N'], showfliers=False, showmeans=True)
+    ax.boxplot(matrix, labels=['D', 'D+C', 'D+T', 'D+U', 'D/N', 'D+C/N', 'D+T/N', 'D+U/N'], showfliers=False, showmeans=True)
     ax.set_ylabel('absolute percentile error')
 
     means = [np.mean(x) for x in matrix]
@@ -84,77 +85,91 @@ if __name__ == '__main__':
         lower_watch_prec = read_as_float_array(fin.readline().rstrip(), delimiter=',')
     bin_num = len(mean_watch_prec)
 
-    # input_loc = 'predict_results/predict_duration.txt'
-    input_loc = 'predict_results/predict_dur_topic.txt'
-
+    # input_loc = 'predict_results/predict_matrix.txt'
+    input_loc = 'predict_results/predict_matrix2.txt'
     ape_matrix = []
+
     dur_err = []
     dur_err_norm = []
-    dur_topic_err = []
-    dur_topic_err_norm = []
 
-    true_err_tuple = []
-    dur_err_tuple = []
-    dur_topic_err_tuple = []
+    content_err = []
+    content_err_norm = []
 
-    cnt1 = 0
-    cnt2 = 0
+    topic_err = []
+    topic_err_norm = []
+
+    channel_err = []
+    channel_err_norm = []
+
+    true_dur_wp_tuple = []
+    dur_dur_wp_tuple = []
+    content_dur_wp_tuple = []
+    topic_dur_wp_tuple = []
+    channel_dur_wp_tuple = []
 
     with open(input_loc, 'r') as fin:
+        fin.readline()
         for line in fin:
-            try:
-                splitted_data = line.rstrip().split('\t')
-                vid, duration, definition, category_id, channel_id, topics, total_view, true_wp, dur_wp, topic_wp, cat_wp, user_wp = line.rstrip().split('\t')
-                duration = int(duration)
-                bin_idx = min(np.sum(duration_gap < duration), bin_num - 1)
-                mean_wp = get_mean_prec(bin_idx)
-                median_wp = get_median_prec(bin_idx)
-                upper_wp = get_upper_bound(bin_idx)
-                lower_wp = get_lower_bound(bin_idx)
+            vid, duration, _, _, _, _, _, _, true_wp, dur_wp, content_wp, topic_wp, channel_wp = line.rstrip().split('\t')
+            duration = int(duration)
+            bin_idx = min(np.sum(duration_gap < duration), bin_num - 1)
+            mean_wp = get_mean_prec(bin_idx)
+            median_wp = get_median_prec(bin_idx)
+            upper_wp = get_upper_bound(bin_idx)
+            lower_wp = get_lower_bound(bin_idx)
 
-                bin_range = upper_wp - lower_wp
-                true_wp = float(true_wp)
-                true_err_tuple.append((duration, true_wp))
+            bin_range = upper_wp - lower_wp
+            true_wp = float(true_wp)
+            true_dur_wp_tuple.append((duration, true_wp))
 
-                if dur_wp != 'NA':
-                    dur_wp = float(dur_wp)
-                    dur_err_tuple.append((duration, dur_wp))
-                    dur_err.append(abs(true_wp - dur_wp) * 100)
-                    dur_err_norm.append(abs(true_wp - dur_wp) / bin_range * 100)
+            if dur_wp != 'NA':
+                dur_wp = float(dur_wp)
+                dur_dur_wp_tuple.append((duration, dur_wp))
+                dur_err.append(abs(true_wp - dur_wp) * 100)
+                dur_err_norm.append(abs(true_wp - dur_wp) / bin_range * 100)
 
-                if topic_wp != 'NA':
-                    topic_wp = float(topic_wp)
-                    dur_topic_err_tuple.append((duration, topic_wp))
-                    dur_topic_err.append(abs(true_wp-topic_wp) * 100)
-                    dur_topic_err_norm.append(abs(true_wp-topic_wp)/bin_range*100)
-                # dur_topic_err.append(abs(true_wp-topic_wp)/bin_range*100)
-                # if abs(true_wp-dur_wp) < abs(true_wp-topic_wp):
-                #     cnt1 += 1
-                # elif abs(true_wp-dur_wp) > abs(true_wp-topic_wp):
-                #     cnt2 += 1
-                # print('dur err: {0:.4f}; topic error: {1:.4f} in range {2:.4f}'.format(abs(true_wp-dur_wp), abs(true_wp-topic_wp), bin_range))
-                # print('norm dur err: {0:.4f}; norm topic error: {1:.4f}'.format(abs(true_wp-dur_wp)/bin_range, abs(true_wp-topic_wp)/bin_range))
-            except Exception as e:
-                print(str(e))
-                break
+            if content_wp != 'NA':
+                content_wp = float(content_wp)
+                content_dur_wp_tuple.append((duration, content_wp))
+                content_err.append(abs(true_wp-content_wp) * 100)
+                content_err_norm.append(abs(true_wp-content_wp)/bin_range*100)
 
-    # print('topic work!: {0}\ntopic not work!: {1}'.format(cnt2, cnt1))
+            if topic_wp != 'NA':
+                topic_wp = float(topic_wp)
+                topic_dur_wp_tuple.append((duration, topic_wp))
+                topic_err.append(abs(true_wp-topic_wp) * 100)
+                topic_err_norm.append(abs(true_wp-topic_wp)/bin_range*100)
+
+            if channel_wp != 'NA':
+                channel_wp = float(channel_wp)
+                channel_dur_wp_tuple.append((duration, channel_wp))
+                channel_err.append(abs(true_wp - channel_wp) * 100)
+                channel_err_norm.append(abs(true_wp - channel_wp) / bin_range * 100)
+
+    print('r2 of duration', r2_score([x[1] for x in true_dur_wp_tuple], [x[1] for x in dur_dur_wp_tuple]))
+    print('r2 of content', r2_score([x[1] for x in true_dur_wp_tuple], [x[1] for x in content_dur_wp_tuple]))
+    print('r2 of topic', r2_score([x[1] for x in true_dur_wp_tuple], [x[1] for x in topic_dur_wp_tuple]))
+    print('r2 of channel', r2_score([x[1] for x in true_dur_wp_tuple], [x[1] for x in channel_dur_wp_tuple]))
 
     ape_matrix.append(dur_err)
-    ape_matrix.append(dur_topic_err)
+    ape_matrix.append(content_err)
+    ape_matrix.append(topic_err)
+    ape_matrix.append(channel_err)
+
     ape_matrix.append(dur_err_norm)
-    ape_matrix.append(dur_topic_err_norm)
-    print(len(dur_err))
-    print(len(dur_topic_err))
-    print(len(dur_err_norm))
-    print(len(dur_topic_err_norm))
+    ape_matrix.append(content_err_norm)
+    ape_matrix.append(topic_err_norm)
+    ape_matrix.append(channel_err_norm)
+
     print('Number of test videos: {0}'.format(len(dur_err)))
 
     fig = plt.figure(figsize=(10, 10))
     plot_performance_comp(ape_matrix, 0)
-    plot_dur_wp_dist(true_err_tuple, 1, 'true dist')
-    plot_dur_wp_dist(dur_err_tuple, 2, 'dur dist')
-    plot_dur_wp_dist(dur_topic_err_tuple, 3, 'topic dist')
+    plot_dur_wp_dist(true_dur_wp_tuple, 1, 'true dist')
+    plot_dur_wp_dist(dur_dur_wp_tuple, 2, 'duration dist')
+    plot_dur_wp_dist(content_dur_wp_tuple, 3, 'content dist')
+    plot_dur_wp_dist(topic_dur_wp_tuple, 4, 'topic dist')
+    plot_dur_wp_dist(channel_dur_wp_tuple, 5, 'channel dist')
 
     plt.tight_layout()
     plt.show()
