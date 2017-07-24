5d08bf3b202ebc749747af8ef7f66795e581d250
avalanchesiqi <wusiqi.china@gmail.com>
2017-07-24 14:44:02 +1000

    [wp prediction] update content predictor, random forest

--- wp_prediction/predict_from_content.py
+++ wp_prediction/predict_from_content.py
@@ -1,119 +1,99 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from __future__ import division, print_function
+import os
 import pandas as pd
 import numpy as np
+import glob
 import cPickle as pickle
 from sklearn.metrics import mean_absolute_error, r2_score
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import RandomForestRegressor
 
 
-def build_matrix(train_loc, test_loc):
-    x = []
-    y = []
-
-    num_train = 0
-    with open(train_loc, 'r') as fin:
-        for line in fin:
-            vid, duration, definition, category_id, lang, _, _, _, true_wp = line.rstrip().split('\t')
-            x.append([np.log10(int(duration)), definition, category_id, lang])
-            y.append(float(true_wp))
-            num_train += 1
-
-    with open(test_loc, 'r') as fin:
-        for line in fin:
-            vid, duration, definition, category_id, lang, _, _, _, true_wp = line.rstrip().split('\t')
-            x.append([np.log10(int(duration)), definition, category_id, lang])
-            y.append(float(true_wp))
-
-    observations = np.array(x)
-    labels = np.array(y)
-
-    # # do the first column
-    # cat_observations = observations[:, 1]
-    # # enc_label = LabelEncoder()
-    # # cat_observations = enc_label.fit_transform(observations[:, 0])
-    #
-    # # do the others
-    # for i in range(1, observations.shape[1]):
-    #     enc_label = LabelEncoder()
-    #     cat_observations = np.column_stack((cat_observations, enc_label.fit_transform(observations[:, i])))
-    #     print(enc_label.classes_)
-    #
-    # cat_observations_values = cat_observations.astype(float)
-    #
-    # enc_onehot = OneHotEncoder()
-    # cat_observations_data = enc_onehot.fit_transform(cat_observations_values)
-    # cat_observations_data = cat_observations_data.toarray()
-    #
-    # # # add control variable
-    # cat_observations_data = np.column_stack((cat_observations_data, observations[:, 0]))
-
-    train_x = observations[:num_train, :]
-    test_x = labels[:num_train]
-    train_y = observations[num_train:, :]
-    test_y = labels[num_train:]
-
-    return train_x, test_x, train_y, test_y
+def get_wp(duration, percentile):
+    bin_idx = np.sum(duration_split_points < duration)
+    duration_bin = dur_engage_map[bin_idx]
+    percentile = int(round(percentile * 1000))
+    wp_percentile = duration_bin[percentile]
+    return wp_percentile
 
 
-def remove_invalid_prediction(arr):
-    arr[arr > 1] = 1
-    arr[arr < 0] = 0
-    return arr
+def get_wp_list(duration_list, percentile_list):
+    wp_list = []
+    for d, p in zip(duration_list, percentile_list):
+        wp_list.append(get_wp(d, p))
+    return wp_list
 
 
 if __name__ == '__main__':
-    categories = ['activism', 'auto', 'comedy', 'education', 'entertainment', 'film', 'gaming', 'howto', 'movie', 'music', 'news', 'people', 'pets', 'science', 'show', 'sports', 'trailer', 'travel']
-    pred_dict = {}
+    # == == == == == == == == Part 1: Set up experiment parameters == == == == == == == == #
+    # setting parameters
+    dur_engage_str_map = pickle.load(open('dur_engage_map.p', 'rb'))
+    dur_engage_map = {key: list(map(float, value.split(','))) for key, value in dur_engage_str_map.items()}
 
-    for category in categories:
-        print('>>> Train on category {0}'.format(category))
-        train_loc = '../../data/production_data/random_langs/train_data/{0}.txt'.format(category)
-        test_loc = '../../data/production_data/random_langs/test_data/{0}.txt'.format(category)
+    duration_split_points = np.array(dur_engage_map['duration'])
 
-        train_df = pd.read_csv(train_loc, sep='\t', header=None,
-                               names=['vid', 'duration', 'definition', 'category', 'lang', 'channel', 'topics', 'total_view', 'watch_percentage'],
-                               dtype={'duration': int, 'definition': int, 'category': int, 'watch_percentage': np.float32})
-        test_df = pd.read_csv(test_loc, sep='\t', header=None,
-                              names=['vid', 'duration', 'definition', 'category', 'lang', 'channel', 'topics', 'total_view', 'watch_percentage'],
-                              dtype={'duration': int, 'definition': int, 'category': int, 'watch_percentage': np.float32})
+    # == == == == == == == == Part 2: Load dataset == == == == == == == == #
+    predict_result_dict = {}
 
-        train_num = train_df.shape[0]
-        data_df = train_df.append(test_df)
+    train_data = glob.glob(os.path.join('../../data/production_data/random_norm/train_data', '*.txt'))
+    test_data = glob.glob(os.path.join('../../data/production_data/random_norm/test_data', '*.txt'))
 
-        data_df['duration'] = np.log10(data_df['duration'])
+    train_df = pd.concat((pd.read_csv(f, sep='\t', header=0,
+                                      names=['vid', 'duration', 'definition', 'category', 'lang', 'channel', 'topics',
+                                             'total_view', 'true_wp', 'wp_percentile'],
+                                      dtype={'duration': int, 'definition': int, 'category': int,
+                                             'wp_percentile': float}) for f in train_data))
 
-        enc_label = LabelEncoder()
-        data_df['lang'] = enc_label.fit_transform(data_df['lang'])
+    test_df = pd.concat((pd.read_csv(f, sep='\t', header=0,
+                                     names=['vid', 'duration', 'definition', 'category', 'lang', 'channel', 'topics',
+                                            'total_view', 'true_wp', 'wp_percentile'],
+                                     dtype={'duration': int, 'definition': int, 'category': int,
+                                            'wp_percentile': float}) for f in test_data))
 
-        train_df = data_df[:train_num]
-        test_df = data_df[train_num:]
+    print('>>> Finish loading all data!')
+    train_num = train_df.shape[0]
+    print('>>> Number of training observations: {0}'.format(train_num))
+    data_df = train_df.append(test_df)
 
-        cols = ['duration', 'definition', 'category', 'lang']
+    # data_df['duration'] = np.log10(data_df['duration'])
 
-        rf_regressor = RandomForestRegressor(n_estimators=10, min_samples_leaf=20, random_state=42)
-        rf_regressor.fit(train_df[cols], train_df.watch_percentage)
+    enc_label = LabelEncoder()
+    data_df['lang'] = enc_label.fit_transform(data_df['lang'])
 
-        print('>>> Feature importances of duration, definition, category, language: {0}'.format(rf_regressor.feature_importances_))
-        print('>>> Number of features: {0}'.format(rf_regressor.n_features_))
+    train_df = data_df[:train_num]
+    test_df = data_df[train_num:]
 
-        pred_train_y = rf_regressor.predict(train_df[cols])
-        pred_train_y = remove_invalid_prediction(pred_train_y)
+    cols = ['duration', 'definition', 'category', 'lang']
+    rf_regressor = RandomForestRegressor(n_estimators=10, min_samples_leaf=100, random_state=42)
+    rf_regressor.fit(train_df[cols], train_df.wp_percentile)
 
-        pred_test_y = rf_regressor.predict(test_df[cols])
-        pred_test_y = remove_invalid_prediction(pred_test_y)
+    print('>>> Feature importances of duration, definition, category, language: {0}'.format(rf_regressor.feature_importances_))
+    print('>>> Number of features: {0}'.format(rf_regressor.n_features_))
 
-        test_df['content_wp'] = pred_test_y
-        pred_result = test_df[['vid', 'content_wp']]
+    pred_train_y = rf_regressor.predict(train_df[cols])
+    pred_train_dc_wp = get_wp_list(train_df['duration'].tolist(), pred_train_y)
 
-        pred_dict.update(test_df.set_index('vid')['content_wp'].to_dict())
+    pred_test_y = rf_regressor.predict(test_df[cols])
+    pred_test_dc_wp = get_wp_list(test_df['duration'].tolist(), pred_test_y)
 
-        print('>>> Random forest MAE on train set: {0:.4f}'.format(mean_absolute_error(train_df.watch_percentage, pred_train_y)))
-        print('>>> Random forest MAE on test set: {0:.4f}'.format(mean_absolute_error(test_df.watch_percentage, pred_test_y)))
-        print('>>> Random forest R^2 on test set: {0:.4f}'.format(r2_score(test_df.watch_percentage, pred_test_y)))
-        print('='*79)
-        print()
+    test_df['content_wp'] = np.asarray(pred_test_dc_wp)
+    pred_result = test_df[['vid', 'content_wp']]
+
+    predict_result_dict.update(test_df.set_index('vid')['content_wp'].to_dict())
+
+    print('>>> Random forest MAE on train set: {0:.4f}'.format(mean_absolute_error(train_df.true_wp, pred_train_dc_wp)))
+    print('>>> Random forest R2 on train set: {0:.4f}'.format(r2_score(train_df.true_wp, pred_train_dc_wp)))
+    print('>>> Random forest MAE on test set: {0:.4f}'.format(mean_absolute_error(test_df.true_wp, pred_test_dc_wp)))
+    print('>>> Random forest R2 on test set: {0:.4f}'.format(r2_score(test_df.true_wp, pred_test_dc_wp)))
+    print('='*79)
+    print()
 
     # write to txt file
-    print('>>> Number of videos in final test result dict: {0}'.format(len(pred_dict)))
-    pickle.dump(pred_dict, open('predict_results/content_result-100.p', 'wb'))
+    to_write = True
+    if to_write:
+        output_path = 'norm_predict_results/predict_dc.p'
+        print('>>> Number of videos in final test result dict: {0}'.format(len(predict_result_dict)))
+        pickle.dump(predict_result_dict, open(output_path, 'wb'))
