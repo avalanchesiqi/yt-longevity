c50fc216ba053dfac2a15fb1fd5b503d2ecfef19
“avalanchesiqi” <wusiqi.china@gmail.com>
2017-08-04 09:16:12 +1000

    [engagement] update dur-wp map plot

--- engagement/extract_dur_engage_map.py
+++ engagement/extract_dur_engage_map.py
@@ -6,6 +6,8 @@ import os
 import numpy as np
 from collections import defaultdict
 import cPickle as pickle
+from scipy.stats import gaussian_kde
+from matplotlib import gridspec
 import matplotlib.pyplot as plt
 from matplotlib.ticker import FuncFormatter
 
@@ -43,12 +45,12 @@ if __name__ == '__main__':
     # == == == == == == == == Part 1: Set up experiment parameters == == == == == == == == #
     # setting parameters
     categories = ['']
-    bin_volume = 2000
+    bin_volume = 5000
     duration_wp_tuple = []
     duration_stats_dict = defaultdict(int)
 
     # == == == == == == == == Part 2: Load dataset == == == == == == == == #
-    input_loc = '../../data/production_data/tweeted_dataset'
+    input_loc = '../../data/tweeted_dataset_norm/test_data'
 
     if os.path.isdir(input_loc):
         for subdir, _, files in os.walk(input_loc):
@@ -99,7 +101,7 @@ if __name__ == '__main__':
         print('num of bins: {0}'.format(len(x_axis)))
 
     # store duration-engagement map as pickle file, each bin gets cut into 1000 percentiles
-    to_pickle = True
+    to_pickle = False
     if to_pickle:
         dur_engage_map = {}
         dur_engage_map['duration'] = strify(x_axis)
@@ -113,14 +115,40 @@ if __name__ == '__main__':
     # plot wp~dur distribution
     to_plot = True
     if to_plot:
-        fig, ax1 = plt.subplots(1, 1)
+        gs = gridspec.GridSpec(2, 2, width_ratios=[8, 1], height_ratios=[1, 8])
+        fig = plt.figure(figsize=(9, 9))
+        ax1 = plt.subplot(gs[1, 0])
         x_axis = np.log10(np.array(x_axis))
-        ax1.plot(x_axis, [np.percentile(x, 1) for x in bin_matrix], 'g.-', label='9%', zorder=1)
-        ax1.plot(x_axis, [np.percentile(x, 25) for x in bin_matrix], 'g--', label='25%', zorder=1)
-        ax1.plot(x_axis, [np.mean(x) for x in bin_matrix], 'r-', label='Mean', zorder=1)
-        ax1.plot(x_axis, [np.median(x) for x in bin_matrix], 'b--', label='Median', zorder=1)
-        ax1.plot(x_axis, [np.percentile(x, 75) for x in bin_matrix], 'g--', label='75%', zorder=1)
-        ax1.plot(x_axis, [np.percentile(x, 99) for x in bin_matrix], 'g.-', label='91%', zorder=1)
+
+        plot_by_density = False
+        if plot_by_density:
+            density_kernel_y = []
+            max_dense = 0
+            for idx, single_bin in enumerate(bin_matrix[:-1]):
+                density = gaussian_kde(single_bin)
+                # generate a fake range of y values
+                ys = np.arange(0, 1, .01)
+                # fill y values using density class
+                den = density(ys)
+                if max(den) > max_dense:
+                    max_dense = max(den)
+            print('max dense', max_dense)
+
+            for idx, single_bin in enumerate(bin_matrix[:-1]):
+                density = gaussian_kde(single_bin)
+                # generate a fake range of y values
+                ys = np.arange(0, 1, .01)
+                # fill y values using density class
+                den = density(ys)
+                for alpha_idx, alpha_value in enumerate(den[:-1]):
+                    # print(idx, x_axis[idx], x_axis[idx+1], xs[alpha_idx], xs[alpha_idx+1])
+                    ax1.fill_between([x_axis[idx], x_axis[idx+1]], ys[alpha_idx], ys[alpha_idx+1], alpha=den[alpha_idx]/max_dense, facecolor='k')
+
+        for t in np.arange(5, 50, 5):
+            ax1.fill_between(x_axis, [np.percentile(x, 50-t) for x in bin_matrix],
+                             [np.percentile(x, 55-t) for x in bin_matrix], facecolor='b', alpha=(100-2*t)/100, lw=0)
+            ax1.fill_between(x_axis, [np.percentile(x, 45+t) for x in bin_matrix],
+                             [np.percentile(x, 50+t) for x in bin_matrix], facecolor='b', alpha=(100-2*t)/100, lw=0)
 
         def exponent(x, pos):
             'The two args are the value and tick position'
@@ -128,11 +156,40 @@ if __name__ == '__main__':
 
         x_formatter = FuncFormatter(exponent)
         ax1.xaxis.set_major_formatter(x_formatter)
-        ax1.set_xlim([0, 5])
+        ax1.set_xlim([1, 5])
         ax1.set_ylim([0, 1])
         ax1.set_xlabel('Video duration (sec)', fontsize=20)
         ax1.set_ylabel('Watch percentage', fontsize=20)
         ax1.tick_params(axis='both', which='major', labelsize=20)
+        for label in ax1.get_xticklabels()[1::2]:
+            label.set_visible(False)
 
-        plt.tight_layout()
+        df_x = [np.log10(x[0]) for x in duration_wp_tuple]
+        df_y = [x[1] for x in duration_wp_tuple]
+        # KDE for top marginal
+        kde_x = gaussian_kde(df_x)
+        # KDE for right marginal
+        kde_y = gaussian_kde(df_y)
+
+        xmin, xmax = 1, 5
+        ymin, ymax = 0, 1
+        x = np.linspace(xmin, xmax, 100)
+        y = np.linspace(ymin, ymax, 100)
+
+        # Create Y-marginal (right)
+        max_xlim = 1.2 * kde_y(y).max()
+        axr = plt.subplot(gs[1, 1], xticks=[], yticks=[], frameon=False, xlim=(0, max_xlim), ylim=(ymin, ymax))
+        axr.plot(kde_y(y), y, color='k')
+        # axr.plot(kde_y[1](y), y, color='r')
+        # axr.plot(kde_y[2](y), y, color='k')
+
+        # Create X-marginal (top)
+        max_ylim = 1.2 * kde_x(x).max()
+        axt = plt.subplot(gs[0, 0], xticks=[], yticks=[], frameon=False, xlim=(xmin, xmax), ylim=(0, max_ylim))
+        axt.plot(x, kde_x(x), color='k')
+        # axt.plot(x, kde_x[1](x), color='r')
+        # axt.plot(x, kde_x[2](x), color='k')
+
+        # fig.tight_layout(pad=0)
+
         plt.show()
