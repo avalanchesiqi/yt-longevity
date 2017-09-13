a7d73ec6708b85e7fe089b3331b09f0349e5e081
“avalanchesiqi” <wusiqi.china@gmail.com>
2017-09-13 11:22:59 +1000

    update wp regressor, part1

--- wp_regressors/duration_predictor.py
+++ wp_regressors/duration_predictor.py
@@ -7,21 +7,26 @@ from __future__ import print_function
 import os
 import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '../'))
-import numpy as np
+import time, datetime
 import cPickle as pickle
+import numpy as np
 from sklearn.metrics import mean_absolute_error, r2_score
 
 from utils.helper import write_dict_to_pickle
+from utils.converter import to_watch_percentage
 
 
 if __name__ == '__main__':
     # == == == == == == == == Part 1: Set up experiment parameters == == == == == == == == #
-    dur_engage_str_map = pickle.load(open('../engagement/data/tweeted_dur_engage_map.p', 'rb'))
+    start_time = time.time()
+
+    dur_engage_str_map = pickle.load(open('./data/tweeted_dur_engage_map.p', 'rb'))
     dur_engage_map = {key: list(map(float, value.split(','))) for key, value in dur_engage_str_map.items()}
     lookup_durations = np.array(dur_engage_map['duration'])
-    true_value = []
-    predict_value = []
-    predict_result_dict = {}
+
+    test_vids = []
+    true_wp = []
+    guess_wp = []
 
     # == == == == == == == == Part 2: Load dataset == == == == == == == == #
     test_loc = '../../production_data/tweeted_dataset_norm/test_data'
@@ -33,22 +38,27 @@ if __name__ == '__main__':
                 fin.readline()
                 for line in fin:
                     vid, _, duration, dump = line.rstrip().split('\t', 3)
-                    wp30 = float(dump.split('\t')[8])
+                    test_vids.append(vid)
                     duration = int(duration)
-
-                    true_value.append(wp30)
-                    random_guess = np.mean(dur_engage_map[np.sum(lookup_durations < duration)])
-                    predict_value.append(random_guess)
-                    predict_result_dict[vid] = random_guess
+                    wp30 = float(dump.split('\t')[8])
+                    true_wp.append(wp30)
+                    random_guess = 0.5
+                    guess_wp.append(to_watch_percentage(dur_engage_map, duration, random_guess, lookup_keys=lookup_durations))
 
     print('>>> Predict watch percentage on duration...')
-    print('>>> MAE on test set: {0:.4f}'.format(mean_absolute_error(true_value, predict_value)))
-    print('>>> R2 on test set: {0:.4f}'.format(r2_score(true_value, predict_value)))
+    print('>>> MAE on test set: {0:.4f}'.format(mean_absolute_error(true_wp, guess_wp)))
+    print('>>> R2 on test set: {0:.4f}'.format(r2_score(true_wp, guess_wp)))
     print('=' * 79)
+
+    # get running time
+    print('\n>>> Total running time: {0}'.format(str(datetime.timedelta(seconds=time.time() - start_time)))[:-3])
 
     # write to pickle file
     to_write = True
+    true_result_dict = {vid: true for vid, true in zip(test_vids, true_wp)}
+    predict_result_dict = {vid: pred for vid, pred in zip(test_vids, guess_wp)}
     if to_write:
         print('>>> Prepare to write to pickle file...')
-        print('>>> Number of videos in final test result dict: {0}'.format(len(predict_result_dict)))
+        print('>>> Number of videos in final test result dict: {0}'.format(len(test_vids)))
+        write_dict_to_pickle(dict=true_result_dict, path='./output/true_predictor.p')
         write_dict_to_pickle(dict=predict_result_dict, path='./output/duration_predictor.p')
