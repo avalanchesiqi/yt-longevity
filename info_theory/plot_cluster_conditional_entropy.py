95a440ffe048329ec6ad0369b5739e12cbababf8
avalanchesiqi <wusiqi.china@gmail.com>
2017-10-09 12:32:22 +1100

    update entropy scripts and plots

--- info_theory/plot_cluster_conditional_entropy.py
+++ info_theory/plot_cluster_conditional_entropy.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""calculate conditional entropy between topic type and relative engagement, i.e, I(Music; eta),
+"""calculate conditional entropy between topic type and relative engagement, i.e, I(book; eta),
 by constructing 2x20 oc-occurrence matrix
 X                0     1
 Y     0-0.05    10    15
@@ -10,6 +10,8 @@ Y     0-0.05    10    15
    0.95-1.00    30    35
 
 I(X;Y) = sum(P(x, y) * log( P(x, y)/P(x)/P(y) ))
+
+Time: ~3M
 """
 
 from __future__ import division, print_function
@@ -17,6 +19,8 @@ import os, sys, time, datetime, operator
 from collections import defaultdict
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+from matplotlib.ticker import FuncFormatter
 
 
 def safe_log2(x):
@@ -33,7 +37,7 @@ def get_conditional_entropy(topic_eta):
         binned_topic_eta[min(int(eta / bin_gap), bin_num - 1)] += 1
 
     p_Y_given_x1 = [binned_topic_eta[i] / len(topic_eta) for i in range(bin_num)]
-    return -np.sum([p * safe_log2(p) for p in p_Y_given_x1])
+    return -np.sum([p * safe_log2(p) for p in p_Y_given_x1]), [binned_topic_eta[x] for x in sorted(binned_topic_eta.keys())]
 
 
 def _load_data(filepath):
@@ -71,8 +75,8 @@ if __name__ == '__main__':
             mid_type_dict[mid] = type
 
     # == == == == == == == == Part 3: Load dataset == == == == == == == == #
-    # data_loc = '../../production_data/tweeted_dataset_norm/'
-    data_loc = '../../production_data/sample_tweeted_dataset/'
+    data_loc = '../../production_data/tweeted_dataset_norm/'
+    # data_loc = '../../production_data/sample_tweeted_dataset/'
     type_eta_dict = defaultdict(list)
     type_eta_counter_dict = defaultdict(int)
     print('>>> Start to load all tweeted dataset...')
@@ -83,15 +87,14 @@ if __name__ == '__main__':
     print('number of types: {0}'.format(len(type_eta_dict)))
 
     # == == == == == Part 4: Calculate conditional entropy for topic type and relative engagement == == == == == #
-    # only count 1000 largest types
-    sorted_type_eta_counter = sorted(type_eta_counter_dict.items(), key=operator.itemgetter(1), reverse=True)[:1000]
-    print('largest 100 clusters')
+    sorted_type_eta_counter = sorted(type_eta_counter_dict.items(), key=operator.itemgetter(1), reverse=True)[:500]
+    print('largest 500 clusters')
     print(sorted_type_eta_counter)
 
     type_conditional_entropy_dict = {}
     for type, _ in sorted_type_eta_counter:
         # type size, conditional entropy, mean eta value
-        type_conditional_entropy_dict[type] = (type_eta_counter_dict[type], get_conditional_entropy(type_eta_dict[type]), np.mean(type_eta_dict[type]))
+        type_conditional_entropy_dict[type] = (type_eta_counter_dict[type], get_conditional_entropy(type_eta_dict[type])[0], np.mean(type_eta_dict[type]))
 
     # get running time
     print('\n>>> Total running time: {0}'.format(str(datetime.timedelta(seconds=time.time() - start_time)))[:-3])
@@ -99,53 +102,88 @@ if __name__ == '__main__':
     # == == == == == == == == Part 5: Generate scatter plots == == == == == == == == #
     to_plot = True
     if to_plot:
-        fig = plt.figure(figsize=(8, 6))
-        ax1 = fig.add_subplot(111)
-        cornflower_blue = '#6495ed'
-        tomato = '#ff6347'
+        plt.figure(figsize=(8, 6))
+        cornflower_blue = (0.3921, 0.5843, 0.9294)
+        tomato = (1.0, 0.3882, 0.2784)
+
+        def make_colormap(seq):
+            """Return a LinearSegmentedColormap
+            seq: a sequence of floats and RGB-tuples. The floats should be increasing
+            and in the interval (0,1).
+            """
+            seq = [(None,) * 3, 0.0] + list(seq) + [1.0, (None,) * 3]
+            cdict = {'red': [], 'green': [], 'blue': []}
+            for i, item in enumerate(seq):
+                if isinstance(item, float):
+                    r1, g1, b1 = seq[i - 1]
+                    r2, g2, b2 = seq[i + 1]
+                    cdict['red'].append([item, r1, r2])
+                    cdict['green'].append([item, g1, g2])
+                    cdict['blue'].append([item, b1, b2])
+            return mcolors.LinearSegmentedColormap('CustomMap', cdict)
+        c = mcolors.ColorConverter().to_rgb
+        rvb = make_colormap([cornflower_blue, c('white'), 0.5, c('white'), tomato])
 
         keys = type_conditional_entropy_dict.keys()
         x_axis = [type_conditional_entropy_dict[x][0] for x in keys]
         y_axis = [type_conditional_entropy_dict[x][1] for x in keys]
-        alphas = [type_conditional_entropy_dict[x][2] for x in keys]
-        ax1.scatter(x_axis, y_axis, facecolors='none', edgecolors='k')
+        colors = [type_conditional_entropy_dict[x][2] for x in keys]
+        plt.scatter(x_axis, y_axis, c=colors, edgecolors='grey', cmap=rvb)
 
-        ax1.set_xscale('log')
-        ax1.set_ylim(ymin=0)
-        ax1.set_xlabel('Cluster size', fontsize=16)
-        ax1.set_ylabel('Conditional entropy', fontsize=16)
+        # plot 10 most determined category
+        top10_index = np.argsort(y_axis)[:10]
+        for idx in top10_index:
+            plt.text(x_axis[idx], y_axis[idx], keys[idx], ha='left', va='center')
+
+        plt.xscale('log')
+        # plt.ylim(ymin=np.floor(min(y_axis)))
+        plt.ylim(ymax=-np.sum([p * safe_log2(p) for p in [bin_gap]*bin_num]))
+        plt.tick_params(axis='both', which='major', labelsize=14)
+        plt.xlabel('Cluster size', fontsize=16)
+        plt.ylabel('Conditional entropy', fontsize=16)
+
+        cb = plt.colorbar()
+        cb.set_label(label='relative engagement $\eta$', size=16)
+        cb.ax.tick_params(labelsize=14)
+        plt.tight_layout(rect=[0.02, 0, 0.98, 1])
+        plt.show()
+
+    to_plot_verbose = True
+    if to_plot_verbose:
+        ax1 = plt.figure(figsize=(8, 6)).add_subplot(111)
+        width = 1 / 2
+        ind = np.arange(20)
+        tomato = '#ff6347'
+        cornflower_blue = '#6495ed'
+        high_engage_topic = 'obamabase'
+        low_engage_topic = 'handcraft'
+
+        count_freq_high = get_conditional_entropy(type_eta_dict[high_engage_topic])[1]
+        prob_high = [x / np.sum(count_freq_high) for x in count_freq_high]
+        ax1.bar(ind + width * 1 / 2, prob_high, width, edgecolor='k', color=tomato, label='{0}'.format(high_engage_topic))
+
+        count_freq_low = get_conditional_entropy(type_eta_dict[low_engage_topic])[1]
+        prob_low = [x / np.sum(count_freq_low) for x in count_freq_low]
+        ax1.bar(ind + width * 3 / 2, prob_low, width, edgecolor='k', color=cornflower_blue, label='{0}'.format(low_engage_topic))
+
+        ax1.set_xlim([0, 20])
+        ax1.set_ylim([0, 0.25])
+        ax1.set_xticks([0, 4, 8, 12, 16, 20])
+
+        def rescale(x, pos):
+            'The two args are the value and tick position'
+            return '%1.1f' % (x / 20)
+
+        formatter = FuncFormatter(rescale)
+
+        ax1.xaxis.set_major_formatter(formatter)
+        ax1.set_xlabel('Relative engagement $\eta$', fontsize=16)
+        ax1.set_ylabel('Conditional probability $P(Y|X=1)$', fontsize=16)
+        ax1.tick_params(axis='both', which='major', labelsize=16)
+        ax1.spines['right'].set_visible(False)
+        ax1.spines['top'].set_visible(False)
+        ax1.set_title('$H(Y|X=1)$; Y=$\eta$, X=topic occurs', fontsize=18)
+        ax1.legend(loc='upper left', fontsize=14, frameon=False)
 
         plt.tight_layout()
         plt.show()
-
-
-# import numpy as np
-# import matplotlib.pyplot as plt
-# import matplotlib.colors as mcolors
-#
-#
-# def make_colormap(seq):
-#     """Return a LinearSegmentedColormap
-#     seq: a sequence of floats and RGB-tuples. The floats should be increasing
-#     and in the interval (0,1).
-#     """
-#     seq = [(None,) * 3, 0.0] + list(seq) + [1.0, (None,) * 3]
-#     cdict = {'red': [], 'green': [], 'blue': []}
-#     for i, item in enumerate(seq):
-#         if isinstance(item, float):
-#             r1, g1, b1 = seq[i - 1]
-#             r2, g2, b2 = seq[i + 1]
-#             cdict['red'].append([item, r1, r2])
-#             cdict['green'].append([item, g1, g2])
-#             cdict['blue'].append([item, b1, b2])
-#     return mcolors.LinearSegmentedColormap('CustomMap', cdict)
-#
-#
-# c = mcolors.ColorConverter().to_rgb
-# rvb = make_colormap([(0.3921, 0.5843, 0.9294), c('white'), 0.5, c('white'), (1.0, 0.3882, 0.2784)])
-# N = 1000
-# array_dg = np.random.uniform(0, 10, size=(N, 2))
-# colors = np.random.uniform(0, 1, size=(N,))
-# plt.scatter(array_dg[:, 0], array_dg[:, 1], c=colors, cmap=rvb)
-# plt.colorbar()
-# plt.show()
--- info_theory/plot_topic_eta_conditional_entropy_dist.py
+++ info_theory/plot_topic_eta_conditional_entropy_dist.py
@@ -11,68 +11,71 @@ def safe_log2(x):
         return np.log2(x)
 
 
-def get_conditional_entropy(engagement_col, topic_eta):
-    bin_gap = 0.05
-    bin_num = int(1 / bin_gap)
+bin_gap = 0.05
+bin_num = int(1 / bin_gap)
 
+
+def get_conditional_entropy(topic_eta):
     # calculate condition entropy when topic appears
     binned_topic_eta = {i: 0 for i in range(bin_num)}
     for eta in topic_eta:
         binned_topic_eta[min(int(eta / bin_gap), bin_num - 1)] += 1
-    n = np.sum(engagement_col)
+    # n = np.sum(engagement_col)
 
-    p_x1 = len(topic_eta) / n
+    # p_x1 = len(topic_eta) / n
     p_Y_given_x1 = [binned_topic_eta[i] / len(topic_eta) for i in range(bin_num)]
-    return -p_x1 * np.sum([p * safe_log2(p) for p in p_Y_given_x1])
+    return -np.sum([p * safe_log2(p) for p in p_Y_given_x1])
+
+
+def get_conditional_entropy2(binned_topic_eta):
+    # p_x1 = len(topic_eta) / n
+    n = np.sum(binned_topic_eta)
+    p_Y_given_x1 = [i / n for i in binned_topic_eta]
+    return -np.sum([p * safe_log2(p) for p in p_Y_given_x1])
 
 
 def rescale(x, pos):
     'The two args are the value and tick position'
-    return '%1.2f' % (x/20)
+    return '%1.1f' % (x/20)
 formatter = FuncFormatter(rescale)
 
-# overall = [225045, 242841, 252768, 245113, 251387, 256861, 252240, 247357, 252111, 251227, 249757, 255001, 244762, 254248, 243531, 247370, 245979, 244484, 245814, 305667]
-# music = {0: 75892, 1: 79314, 2: 78985, 3: 74869, 4: 74375, 5: 73065, 6: 69923, 7: 65947, 8: 65019, 9: 62047, 10: 59424, 11: 57922, 12: 53036, 13: 53190, 14: 49020, 15: 48196, 16: 46128, 17: 44365, 18: 43538, 19: 84429}
-# # Minecraft
-# minecraft = {0: 13557, 1: 13473, 2: 11712, 3: 9513, 4: 8608, 5: 7750, 6: 7059, 7: 6545, 8: 6265, 9: 6129, 10: 6069, 11: 6130, 12: 5745, 13: 5909, 14: 5546, 15: 5532, 16: 5242, 17: 5080, 18: 4823, 19: 7502}
-# music = [music[k] for k in range(20)]
-# residual = [i-j for i, j in zip(overall, music)]
+target1 = [0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 3, 3, 3, 6, 5, 6, 17, 42, 121, 1274]
+topic1 = 'Esther Hicks'
+n1 = np.sum(target1)
+prob_target1 = [x/n1 for x in target1]
 
-# target = {0: 1, 1: 4, 2: 7, 3: 7, 4: 12, 5: 13, 6: 16, 7: 25, 8: 23, 9: 42, 10: 39, 11: 57, 12: 62, 13: 72, 14: 92, 15: 104, 16: 104, 17: 141, 18: 174, 19: 105}
-target = {0: 0, 1: 0, 2: 0, 3: 0, 4: 1, 5: 0, 6: 1, 7: 1, 8: 1, 9: 0, 10: 3, 11: 3, 12: 3, 13: 6, 14: 5, 15: 6, 16: 17, 17: 42, 18: 121, 19: 1274}
-n = np.sum(target.values())
-prob_target = [target[x] / n for x in range(20)]
+target2 = [2819, 853, 394, 214, 118, 74, 53, 27, 18, 17, 10, 4, 10, 6, 1, 3, 1, 1, 1, 16]
+topic2 = 'Android application package'
+n2 = np.sum(target2)
+prob_target2 = [x/n2 for x in target2]
 
-# print(get_conditional_entropy(overall, music))
-
-# overall = np.array(overall)
-# music = np.array(music)
-# residual = np.array(residual)
-# total = np.sum(overall)
-
-# prob_music = music/total
-# prob_residual = residual/total
+target3 = [232, 251, 245, 252, 231, 227, 242, 246, 234, 268, 266, 244, 225, 287, 241, 247, 227, 257, 209, 230]
+topic3 = 'Final Fantasy'
+n3 = np.sum(target3)
+prob_target3 = [x/n3 for x in target3]
 
 fig = plt.figure(figsize=(8, 6))
-width = 1
+width = 1/3
 ind = np.arange(20)
 ax1 = fig.add_subplot(111)
 cornflower_blue = '#6495ed'
 tomato = '#ff6347'
+sea_green = '#2e8b57'
 
-# ax1.bar(ind+width/2, prob_residual, width, bottom=prob_music, edgecolor='k', color=cornflower_blue, label='Residual of Music videos')
-ax1.bar(ind+width/2, prob_target, width, edgecolor='k', color=cornflower_blue, label='Probability of Traget labelled videos')
+ax1.bar(ind+width*5/2, prob_target1, width, edgecolor='k', color=tomato, label='{0}'.format(topic1))
+ax1.bar(ind+width/2, prob_target2, width, edgecolor='k', color=cornflower_blue, label='{0}'.format(topic2))
+ax1.bar(ind+width*3/2, prob_target3, width, edgecolor='k', color=sea_green, label='{0}'.format(topic3))
 
 ax1.set_xlim([0, 21])
-ax1.set_ylim(ymax=ax1.get_ylim()[1]+0.01)
+ax1.set_ylim([0, 1])
 ax1.set_xticks([0, 4, 8, 12, 16, 20])
 ax1.xaxis.set_major_formatter(formatter)
 ax1.set_xlabel('Relative engagement $\eta$', fontsize=16)
-ax1.set_ylabel('Probability', fontsize=16)
+ax1.set_ylabel('Conditional probability $P(Y|X)$', fontsize=16)
 ax1.tick_params(axis='both', which='major', labelsize=16)
 ax1.spines['right'].set_visible(False)
 ax1.spines['top'].set_visible(False)
-# ax1.set_title('Conditional entropy between "" and $\eta$: {0:.4f}'.format(get_conditional_entropy(overall, music)), fontsize=18)
+ax1.set_title('$H(Y|X=1)$; Y=$\eta$, X=topic occurs', fontsize=18)
 ax1.legend(loc='upper left', fontsize=14, frameon=False)
 
 plt.tight_layout()
